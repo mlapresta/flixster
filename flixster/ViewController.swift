64b5b53aa92fffe15befa2970a1ef5b172144a52
Mae LaPresta <mae.lapresta@gmail.com>
2019-10-18 15:23:23 -0500

    Initial Commit

---
+++ flixster/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  flixster
+//
+//  Created by Mae LaPresta on 10/18/19.
+//  Copyright © 2019 Mae LaPresta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
